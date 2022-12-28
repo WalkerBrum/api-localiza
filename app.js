b45b73688780e73275eb2790c9479b53dd1549cc
My Name <walker.b.lobato@gmail.com>
2022-12-27 23:32:37 -0300

    feat: create conecction with server and mongoDB

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('Olá mundo!')
+const database = require('./configs/database');
+const express = require('express');
+
+database.mongoose
+    .connect(database.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log('Conexão estabelecida com o banco de dados.');
+    }).catch(error => {
+        console.log('Não conseguiu conectar com o banco de dados.', error);
+        process.exit();
+    })
+
+// Criando uma aplicação express
+const app = express();
+
+// Definindo que vamos enviar e receber JSON na requisição
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+app.listen(PORT, HOST, () => {
+    console.log(`Servidor executando na porta ${PORT}`);
+});
+
